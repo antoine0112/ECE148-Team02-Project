d213221e4ab10f74615acda570507f0bf44d0710
Sophons <hml024@ucsd.edu>
2021-05-27 19:14:28 -0700

    working esp32 files

---
+++ Arduino/Ari/main.cpp
@@ -0,0 +1,190 @@
+// Dependancies
+#include <Arduino.h>
+#include <SPIFFS.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <ArduinoJson.h>
+
+// Macros
+#define MESSAGE_PIN 2
+#define RED_PIN 0
+#define BLUE_PIN 15
+#define WIFI_ERROR_BLINKS 1
+#define SPIFFS_ERROR_BLINKS 2
+#define SERIAL_ERROR_BLINKS 3
+#define BLINK_CYCLE 1000
+#define HTTP_PORT 80
+
+// WiFi Credentials
+const char *WIFI_SSID = "ESP32";
+const char *WIFI_PASS = "jetsonucsd";
+
+// Led Component
+struct LED {
+    uint8_t pin;
+    bool    on;
+
+    void update() {digitalWrite(pin, on ? HIGH : LOW);}
+};
+
+
+// LED Instances
+LED message_LED = {MESSAGE_PIN, false };
+LED red_LED = {RED_PIN, false };
+LED blue_LED = {BLUE_PIN, false };
+
+// Server Instances
+AsyncWebServer server(HTTP_PORT);
+AsyncWebSocket ws("/ws");
+
+// LED Blinks
+void blink(LED led, int blinks) {
+    // Active
+    for(int i=0; i<blinks*2-1; i++) {   
+        led.on = !led.on;
+        led.update();   
+        delay(BLINK_CYCLE/(blinks*2-1));
+    }
+    // Rest
+    led.on = !led.on;
+    led.update();       
+    delay(BLINK_CYCLE);
+}
+
+// Serial Peripheral Interface Flash File System
+void initSPIFFS() {
+    if (!SPIFFS.begin()) {
+        // Blink Error
+        while(true) {blink(message_LED, SPIFFS_ERROR_BLINKS);}
+    }
+}
+
+// WiFi Connection
+void initWiFi() {
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+    Serial.printf("Trying to connect [%s] ", WiFi.macAddress().c_str());
+    // Blink While Connecting
+    while(WiFi.status() != WL_CONNECTED) {blink(message_LED, WIFI_ERROR_BLINKS);}
+    Serial.printf(" %s\n", WiFi.localIP().toString().c_str());
+}
+
+// Access Point
+void initAccessPoint() {
+    WiFi.softAP(WIFI_SSID, WIFI_PASS);
+    Serial.println("Creating Access Point... IP: "+WiFi.softAPIP().toString());
+}
+
+// FiLL In HTML LEDs
+String processor(const String &var) {
+    String buttons = "";
+    if(var == "LED_BUTTONS"){
+        String red_state = red_LED.on?"on":"off",
+               blue_state = blue_LED.on?"on":"off";
+        buttons += "<button id='red_LED' class="+red_state+"></button>";
+        buttons += "<button id='blue_LED' class="+blue_state+"></button>";
+    }
+    return buttons;
+}
+
+// Locate HTML Page
+void onRootRequest(AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/index.html", "text/html", false, processor);
+}
+
+// WebSocket Events
+void onEvent(AsyncWebSocket       *server,  //
+             AsyncWebSocketClient *client,  //
+             AwsEventType          type,    // the signature of this function is defined
+             void                 *arg,     // by the `AwsEventHandler` interface
+             uint8_t              *data,    //
+             size_t                len) {   //
+
+    // Connect Event
+    if(type == WS_EVT_CONNECT) {
+        Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+    }
+    // Disconnect Event    
+    else if(type == WS_EVT_DISCONNECT) {
+        Serial.printf("WebSocket client #%u disconnected\n", client->id());
+    }    
+    // Incoming Message
+    else if(type == WS_EVT_DATA) {
+        AwsFrameInfo *info = (AwsFrameInfo*)arg;
+        if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+            // Deseralize Message
+            const uint8_t size = JSON_OBJECT_SIZE(2);
+            StaticJsonDocument<size> json;
+            DeserializationError err = deserializeJson(json, data);
+            // Catch Error
+            if (err) {
+                Serial.print(F("deserializeJson() failed with code "));
+                Serial.println(err.c_str());
+                blink(message_LED, SERIAL_ERROR_BLINKS);
+                return;
+            }
+            // Update LEDs
+            if (strcmp(json["led"], "red_LED") == 0) {
+                red_LED.on = !red_LED.on;
+                json["status"] = red_LED.on? "on": "off";
+            } 
+            else if (strcmp(json["led"], "blue_LED") == 0) {
+                blue_LED.on = !blue_LED.on;
+                json["status"] = blue_LED.on? "on": "off";
+            }
+            else
+            {   Serial.println("Unkown Event Message");
+            }   
+            // Sync Devices
+            char syncData[35];
+            ws.textAll(syncData, serializeJson(json, syncData));
+        }
+    }
+}
+
+// Server
+void initWebServer() {
+    // Web Socket
+    ws.onEvent(onEvent);
+    server.addHandler(&ws);
+    
+    // Web Server
+    server.on("/", onRootRequest);
+    server.serveStatic("/", SPIFFS, "/");
+    server.begin();
+}
+
+// Initialization
+void setup() {
+    // Define LED Outputs
+    pinMode(message_LED.pin, OUTPUT);
+    pinMode(red_LED.pin, OUTPUT);
+    pinMode(blue_LED.pin, OUTPUT);
+    
+    // Activate Serial Monitor 
+    Serial.begin(115200); delay(500);
+  
+    // Start SPIFFS, WiFi, and WebServer
+    initSPIFFS();
+    //initWiFi();
+    initAccessPoint();
+    initWebServer();
+}
+
+// Main Loop
+void loop() {
+    // Start Up Success
+    if(!message_LED.on) {
+        message_LED.on = true;
+        message_LED.update();
+    }
+
+    // Update LEDs
+    red_LED.update();
+    blue_LED.update();
+
+    // Close Lingering WebSockets
+    if(millis()%1000 == 0) {
+        ws.cleanupClients(); 
+    }
+}
--- Arduino/ESP_serialWIP/ESP_serialWIP.ino
+++ Arduino/ESP_serialWIP/ESP_serialWIP.ino
@@ -4,53 +4,53 @@
 int mostRecentHrtbt = 0;
 int timeout = 500; // millis
 
-int direction_pin = 7;
-int pwm_pin = 6;
+int direction_pin = 0;
+int pwm_pin = 2;
 
 //for home testing
 int servoPin = direction_pin;
 int escPin = pwm_pin;
 
 int steering_chn = 3;
-int throttle_chn = 2;
+int throttle_chn = 4;
 
 int throttle_PWMmax = 255;
 int throttle_PWMmin = 0;
-int steering_PWMright = 1;
-int steering_PWMleft = -1;
+int steering_PWMright = 255;
+int steering_PWMleft = 0;
 
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(115200); 
 
 //   wait for serial to start up
   while(!Serial) {
   }
 
-  pinMode(direction_pin, OUTPUT);           // Set the direction pin output
-  pinMode(pwm_pin, OUTPUT);                 // Set the pwm pin output
+  // pinMode(direction_pin, OUTPUT);           // Set the direction pin output
+  // pinMode(pwm_pin, OUTPUT);                 // Set the pwm pin output
 
 
-  // pinMode(escPin, OUTPUT);
-  // ledcSetup(throttle_chn, 60, 12);
-  // ledcAttachPin(servoPin, steering_chn);
+  pinMode(escPin, OUTPUT);
+  ledcSetup(throttle_chn, 5000, 8);
+  ledcAttachPin(escPin, throttle_chn);
+
+  pinMode(servoPin, OUTPUT);
+  ledcSetup(steering_chn, 5000, 8);
+  ledcAttachPin(servoPin, steering_chn);
 }
 
 void pwm(float normalized_throttle,float normalized_steering){
-  // pwm = normalized throttle(-1 to 1) * (pwmMax - pwm min) + pwmMiddle
-  int throttle_pwm = int(normalized_throttle*(throttle_PWMmax - throttle_PWMmin) + (throttle_PWMmax - throttle_PWMmin)/2);
-  int steering_pwm = int(normalized_steering*(steering_PWMright - steering_PWMleft) + (steering_PWMright - steering_PWMleft)/2);
+  // pwm = normalized throttle(-1 to 1) * (pwmMax - pwm min) 
+  
+  int throttle_pwm = int(normalized_throttle*(throttle_PWMmax-throttle_PWMmin));
+  int steering_pwm = int(normalized_steering*(steering_PWMright-steering_PWMleft));
+  Serial.print("steering_pwm=");
+   Serial.print(steering_pwm);
+   Serial.print(" throttle_pwm=");
+   Serial.println(throttle_pwm);
 
-  //just for home testing: 
-  // digitalWrite(direction_pin, steering_pwm);   // Set the motor in motion
-  digitalWrite(direction_pin, 0);   // Set the motor in motion
-  // analogWrite(pwm_pin, throttle_pwm);              // Set the motor in motion
-  Serial.print("ABCsteering_pwm=\n");
-  // Serial.print(steering_pwm);
-  // Serial.print("DEFthrottle_pwm=");
-  // Serial.println(throttle_pwm);
-
-  // ledcWrite(throttle_chn, throttle_pwm);
-  // ledcWrite(steering_chn, steering_pwm);
+  ledcWrite(throttle_chn, throttle_pwm);
+  ledcWrite(steering_chn, steering_pwm);
 }
 
 void loop() {
@@ -58,7 +58,7 @@ void loop() {
   while ( !Serial.available()  ){}
   if ( Serial.available() )
     payload = Serial.readStringUntil( '\n' );
-  const uint8_t size = JSON_OBJECT_SIZE(512);
+  const uint8_t size = JSON_OBJECT_SIZE(1000);
   StaticJsonDocument<size> doc;
   // Serial.print(payload);
 
